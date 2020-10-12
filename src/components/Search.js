a3d9144f2c4432effb2dcbb31ef41cb126084d69
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-12 18:22:42 -0400

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Search = () => {
+const Search = ({ inputVal, setInputVal }) => {
   return (
     <form>
-      <input type="text" />
+      <input 
+        type="text"
+        placeholder="Searching for an image"
+        autoFocusvalue={inputVal}
+        onChabge={(e) => setInputVal(e.target.value)}
+      />
       <button type="submit">Search</button>
     </form>
   );
