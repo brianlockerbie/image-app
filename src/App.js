c40a66c49604e5fc827c3f7b43d717b8252494b8
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-13 12:53:18 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ const App = () => {
 
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [search, setSearch] = useState('');
-  const [inputVal, setInputVal] = useState('');
+  const [search, setSearch] = useState("");
+  const [inputVal, setInputVal] = useState("");
 
-  const handleSearch = e => {
+  const handleSearch = (e) => {
     e.preventDefault();
     setSearch(inputVal);
-    setInputVal('');
-  }
+    setInputVal("");
+  };
 
+  const newImages = (direction) => {
+    if (direction === "next") {
+      setCurrentPage((prevCurrent) => prevCurrent + 1);
+    } else if (direction === "previous" && currentPage !== 1) {
+      setCurrentPage((prevCurrent) => prevCurrent - 1);
+    }
+  };  
+  
   useEffect(() => {
+    window.scrollTo(0, 0);
+    
     fetch(
       `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&per_page=9&page=${currentPage}&pretty=true`
     )
       .then((res) => res.json())
       .then((data) => setImages(data.hits));
-  }, []);
+  }, [search, currentPage]);
 
   return (
     <div className="App">
@@ -31,6 +41,7 @@ const App = () => {
         inputVal={inputVal}
         setInputVal={setInputVal}
         handleSearch={handleSearch}
+        newImages={newImages}
       />
     </div>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 import Card from "./Card";
 
 const Hero = (props) => {
-  const { inputVal, setInputVal, handleSearch, images } = props;
+  const { inputVal, setInputVal, handleSearch, images, newImages } = props;
 
   return (
     <section className="hero">
@@ -14,11 +14,15 @@ const Hero = (props) => {
         handleSearch={handleSearch}
       />
       <Container>
-        <Card images={images}/>
-        <div className="btnContainer">
-          <button>Previous</button>
-          <button>Next</button>
-        </div>
+        <Card images={images} />
+        {images.length === 0 ? (
+          <h1>No images found...</h1>
+        ) : (
+          <div className="btnContainer">
+            <button onClick={() => newImages("previous")}>Previous</button>
+            <button onClick={() => newImages("next")}>Next</button>
+          </div>
+        )}
       </Container>
     </section>
   );
