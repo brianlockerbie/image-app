9c866de07f6adb10a01179f55b83de07c9d7f864
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-13 10:28:43 -0400

    Updated Card.js Added Views, Downloads and Likes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,43 @@
 import React from "react";
 
 const Card = ({ images }) => {
-
-    return(
+    return (
        <>
-            {images.map(image => (
-                console.log(image)
-            ))}
-       </>
-    )
-}
+        {images.map((image) => (
+            <div key={image.id} className="card">
+              <a
+                href={image.largeImageUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={image.webformatURL}
+                  alt="photograph"
+                  className="cardImage"
+                />
+                <div className="cardInfo">
+                  <p classNAme="user">Photo by {image.user}</p>
+                  <p>
+                    <i className="fas fa-eye"></i>
+                    <span>Views:</span>
+                    {image.views}
+                  </p>
+                  <p>
+                    <i className="fas fa-download"></i>
+                    <span>Downloads:</span>
+                    {image.downloads}
+                  </p>
+                  <p>
+                    <i className="fas fa-heart"></i>
+                    <span>Likes:</span>
+                    {image.likes}
+                  </p>
+                </div>
+              </a>
+            </div>
+          ))}
+      </>
+    );
+};
 
 export default Card;
